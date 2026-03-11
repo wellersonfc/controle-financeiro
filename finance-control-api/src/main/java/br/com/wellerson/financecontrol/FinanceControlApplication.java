df0efcb9244444c8ea126e54a1fc500d1d58baaa
Wellerson Ferreira de Carvalho <wellersonfc@gmail.com>
2026-03-11 16:44:37 -0300

    Remove invalid @EnableConfigurationProperties annotation
    Remove a misplaced/mistyped @EnableConfigurationProperties(OpenAiProperties.class); line from FinanceControlApplication (it included a stray semicolon). This cleans up the main application class and resolves the syntax/error caused by the invalid annotation usage.

--- finance-control-api/src/main/java/br/com/wellerson/financecontrol/FinanceControlApplication.java
+++ finance-control-api/src/main/java/br/com/wellerson/financecontrol/FinanceControlApplication.java
@@ -2,7 +2,7 @@ package br.com.wellerson.financecontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@EnableConfigurationProperties(OpenAiProperties.class);
+
 @SpringBootApplication
 public class FinanceControlApplication {
 
